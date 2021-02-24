dd6558bc7464e1df4e7bb0c6847b2ca4058fc39e
sfaiad <sfaiad@uw.edu>
2021-02-24 11:22:30 -0800

    Creating a package

---
+++ R/cats.R
@@ -0,0 +1,18 @@
+#' Expressed your feelings about cats 
+#'
+#' @param love A logical argument indicating whether or not you like cats.
+#'
+#' @return One of two character messages about cat feelings
+#' @export
+#'
+#' @examples cats(TRUE)
+cats <- function(love = TRUE) {
+  if(love == TRUE) {
+    msg <- "I love cats!"
+  }
+  else {
+    msg <- "I am not a cat person."
+  }
+  return(print(msg))
+}
+
