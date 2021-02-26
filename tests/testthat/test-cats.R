b6462a6968035d55921c78baa32fc70514be8ebc
sfaiad <sfaiad@uw.edu>
2021-02-26 10:45:06 -0800

    Added unit tests

---
+++ tests/testthat/test-cats.R
@@ -0,0 +1,11 @@
+test_that("logical operations works", {
+  ## if TRUE
+  expect_equal(cats(TRUE), "I love cats!")
+  ## if alias for TRUE
+  expect_equal(cats(1), "I love cats!")
+  ## if FALSE
+  expect_equal(cats(FALSE), "I am not a cat person.")
+  ## if alias for FALSE
+  expect_equal(cats(0), "I am not a cat person.")
+})
+
